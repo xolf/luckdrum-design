9c8171e2ced831afd08e558119de496837ddf77e
Florian <paypal.xolf@yahoo.com>
2015-04-26 21:38:24 +0200

    0.0.6

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<html lang="en">
-<head>
-	<meta charset='utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <!-- Font Awsome -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Luckdrum</title>
-</head>
-<body>
+<?php include('head.php'); ?>
 	<?php include('nav.html') ?>
 	<div class="container">
 		<?php include('sidebar.php'); ?>
@@ -65,5 +51,4 @@
 			</article>
 		</div>
 	</div>
-</body>
-</html>
+<?php include('foot.php'); ?>
