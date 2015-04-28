a08302785f8cd477586d2369a5b88c7b6840f203
Florian <paypal.xolf@yahoo.com>
2015-04-28 21:38:28 +0200

    Still fixing some bugs

--- index.php
+++ index.php
@@ -1,39 +1,54 @@
-<html lang="en">
-<head>
-	<meta charset='utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <!-- Font Awsome -->
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Luckdrum</title>
-</head>
-<body>
+<?php include ('head.php') ?>
 	<?php include('nav.html') ?>
 	<div class="container">
-		<div class="col-md-4">
-			<nav class="nav-left">
-				<a href=""><i class="fa fa-usd"></i>Adverts</a>  
-				<a href=""><i class="fa fa-envelope-o"></i>Receive Message</a>  
-				<a href=""><i class="fa fa-building-o"></i>Business proposals<span class="badge">2</span></a> 
-				<a href=""><i class="fa fa-users"></i>Communities</a> 
-				<a href=""><i class="fa fa-glass"></i>Event</a> 
-				<a href=""><i class="fa fa-shopping-cart"></i>For sale</a> 
-				<a href=""><i class="fa fa-heart"></i>Love connect</a> 
-				<a href=""><i class="fa fa-user-plus"></i>New Contacts</a> 
-				<a href=""><i class="fa fa-thumbs-o-up"></i>Promotion</a> 
-				<a href=""><i class="fa fa-smile-o"></i>Suggestions</a> 
-				<a href=""><i class="fa fa-car"></i>Show offs</a> 
-				<a href=""><i class="fa fa-retweet"></i>Trades</a> 
-				<a href=""><i class="fa fa-briefcase"></i>Vacancies</a> 
-			</nav>
-		</div>
-		<div class="col-md-7">
+		<?php include('sidebar.php') ?>
+		<div class="col-md-5 card-row">
+			<article class="card">
+				<img src="http://coding.smashingmagazine.com/wp-content/uploads/2010/04/javascript-code-snippet.gif" alt="Image" class="card-image">
+				<h2>Hey fellows, reminder for the Monday Javascript meeting</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+				Sed eget leo nibh. Duis tempor tellus sed mi eleifend rutrum. Phasellus eu tellus ultricies, eleifend magna non, ornare libero.<br>
+				Cras pretium, velit porttitor faucibus efficitur, urna sem mollis nibh, pharetra malesuada libero lectus nec ipsum.<br>
+				Donec sit amet consequat erat, posuere tristique nisl.</p>
+				<a href="" class="read-more">Read more <i class="fa fa-angle-double-right"></i></a>
+				<div class="card-info">	
+					<div class="media">
+  						<div class="media-left media-middle">
+    						<a href="">
+								<img src="http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=100" alt="That's John"> 
+							</a>
+						</div>
+						<div class="media-body">
+							John Mermo
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing. </p>
+							<a href="" class="follow">Follow</a>
+						</div>
+					</div>
+				</div>
+			</article>
+			<article class="card">
+				<img src="http://coding.smashingmagazine.com/wp-content/uploads/2010/04/javascript-code-snippet.gif" alt="Image" class="card-image">
+				<h2>Hey fellows, reminder for the Monday Javascript meeting</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+				Sed eget leo nibh. Duis tempor tellus sed mi eleifend rutrum. Phasellus eu tellus ultricies, eleifend magna non, ornare libero.<br>
+				Cras pretium, velit porttitor faucibus efficitur, urna sem mollis nibh, pharetra malesuada libero lectus nec ipsum.<br>
+				Donec sit amet consequat erat, posuere tristique nisl.</p>
+				<a href="" class="read-more">Read more <i class="fa fa-angle-double-right"></i></a>
+				<div class="card-info">	
+					<div class="media">
+  						<div class="media-left media-middle">
+    						<a href="">
+								<img src="http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=100" alt="That's John"> 
+							</a>
+						</div>
+						<div class="media-body">
+							John Mermo
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing. </p>
+							<a href="" class="follow">Follow</a>
+						</div>
+					</div>
+				</div>
+			</article>
 		</div>
 	</div>
-</body>
-</html>
+<?php include('foot.php') ?>
