95a3c326f3865291538f325b3fd8311e72cee659
Florian <paypal.xolf@yahoo.com>
2015-05-12 19:13:52 +0200

    0.0.29
    Added comentarys

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var cnlVersion = '0.0.3', returnAjaxData;
+var cnlVersion = '0.0.3'
+var returnAjaxData;
 $( document ).ready(function() {
 
     //The Panel part
@@ -41,15 +42,15 @@ $( document ).ready(function() {
           };
     };
     
+    //Fetching data from a json file
     var getMeta = $.ajax({
-                            async:false,
+                            async: true,
                             url: 'https://gist.githubusercontent.com/xolf/2fc9efcd1b979d979ae4/raw/1be89cddb2304c1705dfab7ce37bd2963661db10/names.json',
-                            type:'get',
-                            data:{'GetConfig':'YES'},
-                            dataType:"JSON"
+                            type: 'get',
+                            data: {'GetConfig':'YES'},
+                            dataType: "JSON"
                         }).responseJSON;
-    
-    console.log(getMeta);
+
 
     // https://github.com/xolf/Code-Bowl/blob/master/names.js
 
